87d54565bdc86a0d5344bedadc11a16b1490654c
SunithaSivakumaran <SunithaSivakumaran654@gmail.com>
2026-05-08 22:36:36 +0530

    initial commit

---
+++ conn.php
@@ -0,0 +1,19 @@
+<?php
+
+   
+   $db_host="localhost";
+   $db_name="root";
+   $db_password="";
+   $db="admin";
+
+   
+  try{
+   $conn=mysqli_connect($db_host,$db_name,$db_password,$db);
+   echo "You are succesfully connected";
+   }
+  
+  catch(mysqli_sql_exception){
+   echo "Coud not connect to db";
+  }
+  
+?>
